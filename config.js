735d3cab6eda69eb407a0f2db6dc6d5377975e1a
whtsp_ <hifuruhirz@gmail.com>
2026-09-22 23:32:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ const config = {
         "bg2.jpg"
     ],
 
-    whatsappLink: "https://kereva.heavengiirls.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=236468&s2=2242221&click_id=Sweet_Dreams",
-    telegramLink: "https://kereva.heavengiirls.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=236468&s2=2242221&click_id=Sweet_Dreams",
+    whatsappLink: "https://kereva.heavengiirls.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=236468&s2=2242221&click_id=soled",
+    telegramLink: "https://kereva.heavengiirls.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=236468&s2=2242221&click_id=soled",
 
     minMembers: 10000,
     maxMembers: 15000,
